5e11f8f447620f142f43f212667fee514a9dd953
Joe <itsjoemcbride@gmail.com>
2022-09-02 12:12:12 +0100

    Remove irrelevant comments

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,12 +1,3 @@
-// Scenario:
-//  1. Go to google site
-//  2. Search for the keyword 'Test Automation Learning'
-//  3. Select the link with Udemy course
-//  4. Verify if the Udemy site has opened
-//  5. Search for BDD with Cucumber
-//  6.Click on the course with highest rating from the list of search results
-// Task: Automate the above scenario given using JS and any tool (WebdriverIO or Selenium). The Project should be shared as a GitHub project. Each automation step should have assertions''
-
 describe("Assessment", function () {
   it("checks the heading", async function () {
     await browser.url("https://inspiring-wisp-f3b8d3.netlify.app/");
